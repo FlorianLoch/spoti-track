5170c4cd4eb14e815d34702c959bfd06394bdeaf
Florian Loch <florian.loch@gmail.com>
2017-03-14 00:17:09 +0100

    Fix spotifyAdapter.js

--- tests/spotifyAdapter.test.js
+++ tests/spotifyAdapter.test.js
@@ -6,12 +6,9 @@ console.log(adapter);
 describe("spotifyAdapter", () => {
 
     describe("getCurrentMetadata", () => {
-        it("shall fetch a valid pojso", done => {
-            adapter.getCurrentMetadata().then(metadata => {
+        it("shall fetch a valid pojso", () => {
+            return adapter.getCurrentMetadata().then(metadata => {
                 expect(metadata).to.contain.all.keys(["album", "artist", "title"]);
-                done();
-            }, err => {
-                console.error(err);
             });
         });
     });
