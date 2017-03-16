9f88d4ef898206036df874d30577d9e61e339fa7
Florian Loch <florian.loch@gmail.com>
2017-03-16 18:08:43 +0100

    Add code for computation of HMAC, add test

--- tests/encoder.test.js
+++ tests/encoder.test.js
@@ -3,17 +3,17 @@ const expect = require("chai").expect;
 const encoder = require("../lib/encoder");
 
 describe("encoder", () => {
-    describe("_createBlockFromMessage()", () => {
-        let block;
+    let block;
 
-        beforeEach(() => {
-            block = encoder._createBlockFromMessage({
-                artist: "Somebody famoüß",
-                album: "Some great älbum",
-                title: "Some \\reat\nsöng"
-            });
+    beforeEach(() => {
+        block = encoder._createBlockFromMessage({
+            artist: "Somebody famoüß",
+            album: "Some great älbum",
+            title: "Some \\reat\nsöng"
         });
-        
+    });
+
+    describe("_createBlockFromMessage()", () => {
         it("should create blocks having a size which is a multiple of 16 (byte)", () => {
             expect(block).to.be.an.instanceof(Buffer);
             expect(block.length).to.be.equal(57 + 7);
@@ -26,7 +26,15 @@ describe("encoder", () => {
 
         it("should properly handle encoding", () => {
             const str = block.toString("utf8");
-            expect(str).to.contain("\nSome \\\\reat\\nsöng");            
+            expect(str).to.contain("\nSome \\\\reat\\nsöng");
+        });
+    });
+
+    describe("_addHMAC", () => {
+        it("should attach exactly 256 bit to block", () => {
+            const withHMAC = encoder._addHMAC(block, "MY SECRET KEY");
+
+            expect(withHMAC.length).to.be.equal(/* same as above for message */ 64 + /* 256 bits of HMAC */ 32);
         });
     });
 });
