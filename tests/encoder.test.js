04456db416964324caa322eeab0422d2a9dffbcc
Florian Loch <florian.loch@gmail.com>
2017-03-22 20:26:32 +0100

    Start 'encodeMessage', add tests

--- tests/encoder.test.js
+++ tests/encoder.test.js
@@ -4,13 +4,14 @@ const encoder = require("../lib/encoder");
 
 describe("encoder", () => {
     let block;
+    const samplePOJSO = {
+        artist: "Somebody famoüß",
+        album: "Some great älbum",
+        title: "Some \\reat\nsöng"
+    };
 
     beforeEach(() => {
-        block = encoder._createBlockFromMessage({
-            artist: "Somebody famoüß",
-            album: "Some great älbum",
-            title: "Some \\reat\nsöng"
-        });
+        block = encoder._createBlockFromMessage(samplePOJSO);
     });
 
     describe("_createBlockFromMessage()", () => {
@@ -34,7 +35,17 @@ describe("encoder", () => {
         it("should attach exactly 256 bit to block", () => {
             const withHMAC = encoder._addHMAC(block, "MY SECRET KEY");
 
-            expect(withHMAC.length).to.be.equal(/* same as above for message */ 64 + /* 256 bits of HMAC */ 32);
+            expect(withHMAC.length).to.be.equal( /* same as above for message */ 64 + /* 256 bits of HMAC */ 32);
+        });
+    });
+
+    describe("encodeMessage", () => {
+        it("should just combine _createBlockFromMessage and _addHMAC - at the moment!", () => {
+            const withHMAC = encoder._addHMAC(block, "MY SECRET KEY");
+
+            const encoded = encoder.encodeMessage(samplePOJSO, "MY SECRET KEY");
+            
+            expect(withHMAC).to.deep.equal(encoded);
         });
     });
 });
